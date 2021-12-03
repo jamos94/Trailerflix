179f44f60bd07b38ef10645d9268e58393455668
Jessica Amos <amosjessicar@gmail.com>
2021-12-03 18:06:48 -0500

    cleaned up and confirm client functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import Petey from './Petey.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src= {Petey} width="200" height ="300" alt="Petey from the Little Rascals" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's get started.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
